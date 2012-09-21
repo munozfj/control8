cf6ce52cae99c1c418467157204c2c84b168da5b
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-21 10:37:32 -0300

    Inicio de la aplicacion control8

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3'
+gem 'rails', '3.2.8'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+end
+
+group :production do
+  gem 'pg'
+  gem 'thin'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Control8::Application.config.secret_token = '2a6851ccf24896303c6c8f2e653b7e76dc220fed5a69b23b5bc620c287dbfa6862c5d15a46d0b1d66139636e5ef06f0fe50b2443824f65969fba6b0d146e7fca'
